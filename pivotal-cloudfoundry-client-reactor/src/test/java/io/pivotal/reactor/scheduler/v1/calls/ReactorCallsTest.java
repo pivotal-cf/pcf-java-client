54585b5f0d2bcfc4b4299861e3d4dbeb841525a5
Paul Harris <pharris@pivotal.io>
2018-04-12 11:40:29 +0100

    Support Listing Calls
    This commit adds support for listing calls within a space.

--- pivotal-cloudfoundry-client-reactor/src/test/java/io/pivotal/reactor/scheduler/v1/calls/ReactorCallsTest.java
+++ pivotal-cloudfoundry-client-reactor/src/test/java/io/pivotal/reactor/scheduler/v1/calls/ReactorCallsTest.java
@@ -20,18 +20,25 @@ import io.pivotal.reactor.InteractionContext;
 import io.pivotal.reactor.TestRequest;
 import io.pivotal.reactor.TestResponse;
 import io.pivotal.reactor.scheduler.AbstractSchedulerApiTest;
+import io.pivotal.scheduler.v1.Link;
+import io.pivotal.scheduler.v1.Pagination;
+import io.pivotal.scheduler.v1.calls.CallResource;
 import io.pivotal.scheduler.v1.calls.CreateCallRequest;
 import io.pivotal.scheduler.v1.calls.CreateCallResponse;
 import io.pivotal.scheduler.v1.calls.DeleteCallRequest;
+import io.pivotal.scheduler.v1.calls.ListCallsRequest;
+import io.pivotal.scheduler.v1.calls.ListCallsResponse;
 import org.junit.Test;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
 import static io.netty.handler.codec.http.HttpMethod.DELETE;
+import static io.netty.handler.codec.http.HttpMethod.GET;
 import static io.netty.handler.codec.http.HttpMethod.POST;
 import static io.netty.handler.codec.http.HttpResponseStatus.CREATED;
 import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 public final class ReactorCallsTest extends AbstractSchedulerApiTest {
 
@@ -88,6 +95,55 @@ public final class ReactorCallsTest extends AbstractSchedulerApiTest {
                 .callId("test-call-id")
                 .build())
             .as(StepVerifier::create)
+            .expectComplete()
+            .verify(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void list() {
+        mockRequest(InteractionContext.builder()
+            .request(TestRequest.builder()
+                .method(GET).path("/calls?space_guid=test-space-id")
+                .build())
+            .response(TestResponse.builder()
+                .status(OK)
+                .payload("fixtures/scheduler/v1/calls/GET_{space_id}_response.json")
+                .build())
+            .build());
+
+        this.calls
+            .list(ListCallsRequest.builder()
+                .spaceId("test-space-id")
+                .build())
+            .as(StepVerifier::create)
+            .expectNext(ListCallsResponse.builder()
+                .pagination(Pagination.builder()
+                    .first(Link.builder()
+                        .href("test-first-link")
+                        .build())
+                    .last(Link.builder()
+                        .href("test-last-link")
+                        .build())
+                    .next(Link.builder()
+                        .href("test-next-link")
+                        .build())
+                    .previous(Link.builder()
+                        .href("test-previous-link")
+                        .build())
+                    .totalPages(1)
+                    .totalResults(1)
+                    .build())
+                .resource(CallResource.builder()
+                    .applicationId("test-application-id")
+                    .authorizationHeader("test-authorization-header")
+                    .createdAt("test-created-at")
+                    .id("test-job-id")
+                    .name("test-name")
+                    .spaceId("test-space-id")
+                    .updatedAt("test-updated-at")
+                    .url("test-url")
+                    .build())
+                .build())
             .expectComplete()
             .verify(Duration.ofSeconds(5));
     }
